6c4ff8fa0694a362774c661a030c9af529f2bc4c
Justin Yoo <justin.yoo@aliencube.com>
2013-08-21 21:54:46 +1000

    Refactored /account/login and /home/index. Added login validation on main.js Updated web.config.

--- SourceCodes/MokaCms.Web.UI/Scripts/main.js
+++ SourceCodes/MokaCms.Web.UI/Scripts/main.js
@@ -1,1 +1,26 @@
-﻿
+﻿// $(selector).event(callback)
+
+(function ($) {
+	var documentReady = function () {
+		$("#signin").click(function () {
+			var username = $("#username").val();
+			var password = $("#password").val();
+
+			var submit = false;
+			$.each(users, function (i, user) {
+				var keepgoing = true;
+				if (user.username == username && user.password == password) {
+					submit = true;
+					keepgoing = false;
+				} else {
+					$("body").css("background-color", "red");
+					$("#login-message").html("Username or Password was invalid");
+				}
+				return keepgoing;
+			});
+			return submit;
+		});
+	};
+
+	$(document).ready(documentReady);
+})(jQuery);

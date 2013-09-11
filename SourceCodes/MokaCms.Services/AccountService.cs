2efdb93a3358e1e16b454ff7ae53cd7d99e0e4a3
Justin Yoo <justin.yoo@aliencube.com>
2013-09-11 20:53:26 +1000

    Added a new service project.

---
+++ SourceCodes/MokaCms.Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MokaCms.Services
+{
+	public class AccountService
+	{
+		public bool Login(string username, string password)
+		{
+			var result = username == "robin" && password == "robin";
+
+			return result;
+		}
+	}
+}
---
+++ SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MokaCms.Services;
+using NUnit.Framework;
+
+namespace MokaCms.Web.UI.Tests
+{
+	[TestFixture]
+	public class AccountServiceTest
+	{
+		#region SetUp / TearDown
+
+		[SetUp]
+		public void Init()
+		{ }
+
+		[TearDown]
+		public void Dispose()
+		{ }
+
+		#endregion
+
+		#region Tests
+
+		[Test]
+		[TestCase("robin", "robin", true)]
+		[TestCase("patrick", "hayley", false)]
+		public void Login_GivenUsernamePassword_LoginConfirmed(string username, string password, bool authenticated)
+		{
+			var account = new AccountService();
+			var result = account.Login(username, password);
+
+			Assert.AreEqual(authenticated, result);
+		}
+
+		#endregion
+	}
+}

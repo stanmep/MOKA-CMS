5fdb17be14641a1f84d3fc0b5f65a7f1bdbc6524
Bongmin Shin <bongmin0224@gmail.com>
2013-09-13 23:51:25 +1000

    Fix errors

--- SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
+++ SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
@@ -1,52 +1,8 @@
-<<<<<<< HEAD
-﻿using System;
-=======
 using System;
->>>>>>> cfca9fd5c11a8b213453f436df65df592c854f32
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
-<<<<<<< HEAD
-using Mokacms.Services;
-using NUnit.Framework;
-
-
-namespace MokaCms.Web.UI.Tests
-{
-    [TestFixture]
-    public class AccountServiceTest
-    {
-        #region SetUp / TearDown
-
-        [SetUp]
-        public void Init()
-        { }
-
-        [TearDown]
-        public void Dispose()
-        { }
-
-        #endregion
-
-        #region Tests
-
-        [Test]
-        [TestCase("robin", "robin",true)]
-        [TestCase("Bongmin","bongmin",false)]
-        public void Login_GivenUsernamePassword_LoginConfirmed(string username, string password, bool authenticated)
-        {
-            var account = new AccountService();
-            var result = account.Login(username, password);
-
-            Assert.AreEqual(authenticated, result);
-
-        }
-
-        #endregion
-    }
-
-=======
 using MokaCms.Services;
 using NUnit.Framework;
 
@@ -82,5 +38,4 @@ namespace MokaCms.Web.UI.Tests
 
 		#endregion
 	}
->>>>>>> cfca9fd5c11a8b213453f436df65df592c854f32
 }

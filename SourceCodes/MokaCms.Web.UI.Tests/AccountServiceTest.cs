c6b876906c222230530519ae3f1b376666ff6c88
Bongmin Shin <bongmin0224@gmail.com>
2013-09-13 23:13:16 +1000

    commit

---
+++ SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Mokacms.Services;
+using NUnit.Framework;
+
+
+namespace MokaCms.Web.UI.Tests
+{
+    [TestFixture]
+    public class AccountServiceTest
+    {
+        #region SetUp / TearDown
+
+        [SetUp]
+        public void Init()
+        { }
+
+        [TearDown]
+        public void Dispose()
+        { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        [TestCase("robin", "robin",true)]
+        [TestCase("Bongmin","bongmin",false)]
+        public void Login_GivenUsernamePassword_LoginConfirmed(string username, string password, bool authenticated)
+        {
+            var account = new AccountService();
+            var result = account.Login(username, password);
+
+            Assert.AreEqual(authenticated, result);
+
+        }
+
+        #endregion
+    }
+
+}
---
+++ SourceCodes/Mokacms.Services/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokacms.Services
+{
+    public class AccountService
+    {
+        public bool Login(string username, string password)
+        {
+            var result = false;
+            if (username == "robin" && password == "robin")
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}

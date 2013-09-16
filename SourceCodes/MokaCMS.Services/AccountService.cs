4b64e10a0538d9230a927e1628d42fb636ffbf44
patricklee1014 <patricklee1014+github@gmail.com>
2013-09-17 01:10:18 +1000

    Update MokaCMS.Service

---
+++ SourceCodes/MokaCMS.Services/AccountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MokaCMS.Services
+{
+    public class AccountService
+    {
+        
+           public bool Login(string username, string password)
+           {
+               var result = username == "robin" && password == "robin";
+               return result;
+           }
+        
+    }
+}

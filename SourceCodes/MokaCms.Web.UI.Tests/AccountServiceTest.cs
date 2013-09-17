5d0449b82443b94cf3036c75e507e1ce2640bcac
Justin Yoo <justin.yoo@aliencube.com>
2013-09-17 23:28:14 +1000

    Updated project manifest files.

--- SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
+++ SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
@@ -3,52 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
-<<<<<<< HEAD
-using MokaCMS.Services;
-=======
 using MokaCms.Services;
->>>>>>> cfca9fd5c11a8b213453f436df65df592c854f32
 using NUnit.Framework;
 
 namespace MokaCms.Web.UI.Tests
 {
-<<<<<<< HEAD
-    [TestFixture]
-    public class AccountServiceTest
-    {
-        #region SetUp / TearDown
-
-        // Common executable mothod for testing
-        [SetUp]
-        public void Init()
-        { }
-
-        
-        [TearDown]
-        public void Dispose()
-        { }
-
-        #endregion
-
-        #region Tests
-
-        [Test]
-        [TestCase("robin", "robin", true)]
-        [TestCase("patrick", "robin", false)]
-        public void LoginTest_GivenUsernamePassword_LoginConfirmed(string username, string password, bool authenticated)
-        {
-
-            var account = new AccountService();
-            var result = account.Login(username, password);
-
-            Assert.AreEqual(authenticated, result);
-        }
-
-        #endregion
-    }
-
-   
-=======
 	[TestFixture]
 	public class AccountServiceTest
 	{
@@ -79,5 +38,4 @@ namespace MokaCms.Web.UI.Tests
 
 		#endregion
 	}
->>>>>>> cfca9fd5c11a8b213453f436df65df592c854f32
 }

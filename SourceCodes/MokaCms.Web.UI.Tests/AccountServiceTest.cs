d5583ae20f0a1427cb83dd1b57e4087e4109da55
stanmep <stanmep3@gmail.com>
2013-09-18 18:27:36 +1000

    commit

---
+++ SourceCodes/MokaCms.Web.UI.Tests/AccountServiceTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web.Mvc;
+using MokaCms.Services;
+using NUnit.Framework;
+
+namespace MokaCms.Web.UI.Tests
+{
+    [TestFixture]
+    public class AccountServiceTest
+    {
+        #region SetUp / TearDown
+
+        [SetUp]
+        public void Init()
+        { }
+
+        [TearDown]
+        public void Dispose()
+        { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        [TestCase("robin", "robin", false)]
+        [TestCase("patrick", "hayley", false)]
+        public void LoginTest_GivenUsernamePassword_LoginConfirmed(string username, string password, bool authenticated)
+        {
+
+            var account = new AccountService();
+            var result = account.Login(username, password);
+
+            Assert.AreEqual(authenticated, result);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SourceCodes/MokaCms.Web.UI.Tests/LoginTestService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace MokaCms.Web.UI.Tests
+{
+    [TestFixture]
+    public class LoginTestService
+    {
+        #region SetUp / TearDown
+
+        [SetUp]
+        public void Init()
+        { }
+
+        [TearDown]
+        public void Dispose()
+        { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        #endregion
+    }
+}
